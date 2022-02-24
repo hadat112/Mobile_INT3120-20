e4c3e76d165728751a062990b4d8672afbec99f1
hadat112 <hadat1122001@gmail.com>
2022-02-24 22:11:57 +0700

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -149,25 +149,27 @@ class ProductBox extends StatelessWidget {
         height: 140,
         child: Card(
         child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
-          // Image.asset("assets/appimages/" +image),
-          Expanded(
-              child: Container(
-                  padding: EdgeInsets.all(5), child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-
-                  Text(this.name, style: TextStyle(fontWeight:
-                  FontWeight.bold)), Text(this.description),
-                  Text("Price: " + this.price.toString()),
-                  RatingBox(),
-                ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("assets/appimages/" + image),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(this.name, style: TextStyle(fontWeight:
+                      FontWeight.bold)),
+                      Text(this.description),
+                      Text("Price: " + this.price.toString()),
+                      RatingBox(),
+                    ],
+                  )
+                )
               )
-              )
-          )
-        ]
+            ]
         )
-    )
+        )
     );
   }
 }
