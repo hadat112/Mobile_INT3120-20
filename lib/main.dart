8b23bf971c465a86995d14daad7f39522d73f833
hadat112 <hadat1122001@gmail.com>
2022-02-24 22:15:59 +0700

    statefullwidget

--- lib/main.dart
+++ lib/main.dart
@@ -28,37 +28,37 @@ class MyHomePage extends StatelessWidget {
                 name: "iPhone",
                 description: "iPhone is the stylist phone ever",
                 price: 1000,
-                image: "iphone.jpg"
+                image: "iphone.jpeg"
             ),
             ProductBox(
                 name: "Pixel",
                 description: "Pixel is the most featureful phone ever",
                 price: 800,
-                image: "pixel.jpg"
+                image: "pixel.jpeg"
             ),
             ProductBox(
                 name: "Laptop",
                 description: "Laptop is most productive development tool",
                 price: 2000,
-                image: "laptop.jpg"
+                image: "laptop.jpeg"
             ),
             ProductBox(
                 name: "Tablet",
                 description: "Tablet is the most useful device ever for meeting",
                 price: 1500,
-                image: "tablet.jpg"
+                image: "tablet.jpeg"
             ),
             ProductBox(
                 name: "Pendrive",
                 description: "Pendrive is useful storage medium",
                 price: 100,
-                image: "pendrive.jpg"
+                image: "pendrive.jpeg"
             ),
             ProductBox(
                 name: "Floppy Drive",
                 description: "Floppy drive is useful rescue storage medium",
                 price: 20,
-                image: "floppydisk.jpg"
+                image: "floppydisk.jpeg"
             ),
           ],
         )
@@ -151,7 +151,7 @@ class ProductBox extends StatelessWidget {
         child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Image.asset("assets/appimages/" + image),
+              Image.asset("assets/" + image),
               Expanded(
                 child: Container(
                   padding: EdgeInsets.all(5),
