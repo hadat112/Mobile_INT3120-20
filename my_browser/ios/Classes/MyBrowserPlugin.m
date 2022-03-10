cb76db1814897f17530fbe89b38a69e679cc1b9d
hadat112 <hadat1122001@gmail.com>
2022-03-10 23:43:14 +0700

    Lesson16: package

---
+++ my_browser/ios/Classes/MyBrowserPlugin.m
@@ -0,0 +1,20 @@
+#import "MyBrowserPlugin.h"
+
+@implementation MyBrowserPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"my_browser"
+            binaryMessenger:[registrar messenger]];
+  MyBrowserPlugin* instance = [[MyBrowserPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
